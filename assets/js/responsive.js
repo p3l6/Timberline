3f9667c4c4b134264566305c3a7d55e1076d456c
Paul Landers <paul@srednal.com>
2020-04-08 21:18:53 -0400

    Make menu bar a responsive dropdown

---
+++ assets/js/responsive.js
@@ -0,0 +1,8 @@
+function expandFunction() {
+  var x = document.getElementById("banner");
+  if (x.className === "expanded") {
+    x.className = "";
+  } else {
+    x.className = "expanded";
+  }
+}
